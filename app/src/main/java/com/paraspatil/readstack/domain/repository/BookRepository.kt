de205b2cc7dbbb1d35cf525f393800162b7c5b28
PatilParas05 <parasspatil.111@gmail.com>
2026-01-13 11:56:29 +0530

    Add searchWithPegination

--- app/src/main/java/com/paraspatil/readstack/domain/repository/BookRepository.kt
+++ app/src/main/java/com/paraspatil/readstack/domain/repository/BookRepository.kt
@@ -12,8 +12,17 @@ interface BookRepository {
     suspend fun addBookToLibrary(book: BookEntity)
     suspend fun deleteBook(book: BookEntity)
     suspend fun clearLibrary()
+
+
     fun getSearchResults(query: String): Flow<List<SearchResultEntity>>
     suspend fun searchBooks(query: String): NetworkResult<Unit>
+
+    suspend fun searchBooksWithPagination(
+        query: String,
+        startIndex: Int=0,
+        maxResults: Int=20,
+        shouldReplace: Boolean = true,
+        ): NetworkResult<Unit>
     suspend fun clearSearchCache(query: String)
     suspend fun syncLibrary(): Result<Unit>
     fun isOnline(): Flow<Boolean>
