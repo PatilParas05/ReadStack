d2f004dae9b689102f1696fadac3de554ee81339
PatilParas05 <parasspatil.111@gmail.com>
2026-01-17 18:34:44 +0530

    Add Book Details Screen

--- app/src/main/java/com/paraspatil/readstack/ui/details/BookDetailScreen.kt
+++ app/src/main/java/com/paraspatil/readstack/ui/details/BookDetailScreen.kt
@@ -1,2 +1,100 @@
 package com.paraspatil.readstack.ui.details
 
+import androidx.collection.objectLongMapOf
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookDetailScreen(
+
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Book Details") },
+                navigationIcon ={
+                    IconButton(onClick = { /*TODO*/ }){
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = "https://picsum.photos/200/300",
+                contentDescription = "Book cover",
+                modifier = Modifier.height(300.dp).fillMaxWidth(),
+                contentScale= ContentScale.Fit
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineMedium,
+                text = "Book Title"
+            )
+            Spacer(modifier=Modifier.height(4.dp))
+            Text(
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium,
+                text = "Author"
+            )
+            Spacer(modifier = Modifier .height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = "Page Count: 200",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = "No description available",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun BookDetailScreenPreview(){
+    BookDetailScreen()
+}
